ff37b8eed52b4ea5b14f3a609c6b57b8eee05ee3
wailorman <wailorman@gmail.com>
2015-12-01 02:55:15 +0300

    feat(dexie adapter): Add collection interface + other improvements

---
+++ test/unit/lib/backbone-dexie-adapter/fixtures.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Milk',
+        price: 100
+    },
+    {
+        id: 2,
+        name: 'Water',
+        price: 10
+    },
+    {
+        id: 3,
+        name: 'Cola',
+        price: 50
+    }
+];
