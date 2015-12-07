8063814edb51715c64af95a3cbba1e7f4e9a3f20
wailorman <wailorman@gmail.com>
2015-12-07 18:06:52 +0300

    chore: Add partial (harmony) ES6 support in tests

--- test/babel-opts.js
+++
@@ -1,8 +0,0 @@
-require('babel-register')({
-    presets: ['es2015'],
-    sourceMaps: true,
-    //experimental: true,
-    retainLines: true
-});
-
-global.indexedDB = require('fake-indexeddb');
---
+++ test/preconf.js
@@ -0,0 +1,10 @@
+global.indexedDB = require('fake-indexeddb');
+
+global.chai = require('chai');
+global.expect = chai.expect;
+global.assert = chai.assert;
+global.sinon = require('sinon');
+
+global._ = require('underscore');
+global.Q = require('q');
+global.async = require('async');
