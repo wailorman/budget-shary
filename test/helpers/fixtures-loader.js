374016d6baaf8e3dfdff111c5b91bfabc14a5162
wailorman <wailorman@gmail.com>
2015-12-07 18:09:39 +0300

    chore: fixturesLoader.forMultiplieTables(...)

--- test/helpers/fixtures-loader.js
+++ test/helpers/fixtures-loader.js
@@ -1,6 +1,11 @@
-var Dexie = require('dexie');
-var async = require('async');
-var Q = require('q');
+"use strict";
+
+const Dexie = require('dexie');
+const async = require('async');
+const Q = require('q');
+const _ = require('underscore');
+
+const sandboxDB = new Dexie('SandboxDatabase');
 
 var fixturesLoader = function (dexieTable, fixturesArray) {
     var deferred = Q.defer();
@@ -24,24 +29,87 @@ var fixturesLoader = function (dexieTable, fixturesArray) {
     return deferred.promise;
 };
 
-var _createSandboxTable = function(schema) {
+/**
+ * @param db Dexie   database
+ * @param {Object} payload   Object fixtures for loading ( { <tableName>: fixturesArray[] } )
+ */
+fixturesLoader.forMultiplieTables = fixturesLoader._routePayload;
+
+fixturesLoader._createSandboxTable = function (schema) {
 
     schema = schema || "++id,name";
 
-    var db = new Dexie('SandboxDatabase');
+    sandboxDB.delete();
 
-    db.delete();
-
-    db.version(1).stores({
+    sandboxDB.version(1).stores({
         sandbox: schema
     });
 
-    db.open();
+    sandboxDB.open();
 
-    return db.sandbox;
+    return sandboxDB.sandbox;
 
 };
 
-fixturesLoader._createSandboxTable = _createSandboxTable;
+fixturesLoader._routePayload = function (db, payload) {
+
+    var deferred = Q.defer();
+
+    /*
+     * payload schema:
+     * { <tableName>: fixturesArray[] }
+     *
+     * */
+
+    let numberOfTablesInPayload = _.keys(payload).length;
+
+    async.times(numberOfTablesInPayload, function (i, next) {
+
+        let tableName = _.keys(payload)[i];
+        let table = db[tableName];
+        let fixtures = payload[tableName];
+
+        fixturesLoader._loadFixturesToTable(table, fixtures)
+            .then(()=> {
+                next();
+            })
+            .catch(next);
+
+    }, function (err) {
+        if (err) deferred.reject(err);
+        else deferred.resolve(null);
+    });
+
+    return deferred.promise;
+
+};
+
+fixturesLoader._clearTable = function (dexieTable) {
+
+    return dexieTable.clear();
+
+};
+
+fixturesLoader._loadFixturesToTable = function (dexieTable, fixturesArray) {
+
+    var deferred = Q.defer();
+
+    async.each(fixturesArray, function (fixture, callback) {
+
+        dexieTable.add(fixture)
+            .then(function () {
+                callback();
+            }, function (err) {
+                callback(err);
+            });
+
+    }, function (err) {
+        if (err) deferred.reject(err);
+        else deferred.resolve(null);
+    });
+
+    return deferred.promise;
+
+};
 
 module.exports = fixturesLoader;
--- test/unit/lib/fixtures-loader.test.js
+++ test/unit/lib/fixtures-loader.test.js
@@ -1,19 +1,157 @@
-global.indexedDB = require('fake-indexeddb');
+"use strict";
 
 var fixturesLoader = require('../../helpers/fixtures-loader');
 var Dexie = require('dexie');
 
-var chai = require('chai');
-var assert = chai.assert;
-var expect = chai.expect;
-
 describe('fixturesLoader', function () {
 
-    var _createSandboxTable;
+    var _createSandboxTable,
+        _loadFixturesToTable,
+        _routePayload;
 
     before(function () {
 
         _createSandboxTable = fixturesLoader._createSandboxTable;
+        _loadFixturesToTable = fixturesLoader._loadFixturesToTable;
+        _routePayload = fixturesLoader._routePayload;
+
+    });
+
+    describe("_routePayload", ()=> {
+
+        let sandboxDB, payload, spies = {};
+
+        let prepareSandboxDatabase = function () {
+
+            sandboxDB = new Dexie('TestingRoutePayloadDB');
+
+            return sandboxDB.delete()
+                .then(() => {
+
+                    sandboxDB.version(1).stores({
+                        first: '++id,name',
+                        second: '++id,name',
+                        third: '++id,name'
+                    });
+
+                    return sandboxDB.open();
+
+                });
+        };
+
+
+        payload = {
+            first: [
+                {
+                    id: 1,
+                    name: '1-First'
+                },
+                {
+                    id: 2,
+                    name: '2-First'
+                }
+            ],
+            second: [
+                {
+                    id: 1,
+                    name: '1-Second'
+                },
+                {
+                    id: 2,
+                    name: '2-Second'
+                }
+            ]
+        };
+
+
+        before(()=> {
+
+            return prepareSandboxDatabase().then(()=> {
+
+                spies.first = sinon.spy(sandboxDB.first, "add");
+                spies.second = sinon.spy(sandboxDB.second, "add");
+                spies.third = sinon.spy(sandboxDB.third, "add");
+
+                spies._loadFixturesToTable = sinon.spy(fixturesLoader, "_loadFixturesToTable");
+
+            });
+
+        });
+
+        beforeEach(() => {
+
+            spies.first.reset();
+            spies.second.reset();
+            spies.third.reset();
+            spies._loadFixturesToTable.reset();
+
+        });
+
+        it("should call _loadFixturesToTable", () => {
+
+            let simplePayload = {
+                first: [{
+                    id: 1,
+                    name: 'First'
+                }]
+            };
+
+            return _routePayload(sandboxDB, simplePayload)
+                .then(()=> {
+
+                    assert.isTrue(spies._loadFixturesToTable.calledWith(sandboxDB.first, simplePayload.first),
+                        "_loadFixturesToTable called with wrong arguments");
+
+                });
+
+        });
+
+        it("should pass fixtures to the right tables", () => {
+
+            _routePayload(sandboxDB, payload);
+
+            assert(spies.third.callCount === 0, "'third' table shouldn't be touched");
+            assert.isTrue(spies.first.calledTwice, "'first' table wasn't touched");
+            assert.isTrue(spies.second.calledTwice, "'second' table wasn't touched");
+
+        });
+
+    });
+
+    describe("_loadFixturesToTable()", ()=> {
+
+        let sandboxTable;
+        let fixturesArray;
+
+        beforeEach(() => {
+            sandboxTable = _createSandboxTable();
+
+            fixturesArray = [
+                {
+                    id: 1,
+                    name: 'First'
+                },
+                {
+                    id: 2,
+                    name: 'Second'
+                }
+            ];
+        });
+
+        it("should load fixtures array to table", () => {
+
+            return _loadFixturesToTable(sandboxTable, fixturesArray)
+                .then(() => {
+                    return sandboxTable.toArray()
+                })
+                .then((sandboxTableContent) => {
+
+                    expect(sandboxTableContent[0].id).to.equal(fixturesArray[0].id);
+                    expect(sandboxTableContent[1].id).to.equal(fixturesArray[1].id);
+
+                });
+
+        });
 
     });
 
