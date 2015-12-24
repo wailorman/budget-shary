3920d77f4f0d5fbec5551db3b2e41bc06ee36a81
wailorman <wailorman@gmail.com>
2015-12-24 16:40:16 +0300

    feat(components): Add NewPersonButton component

---
+++ test/unit/components/NewPersonButton.test.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const NewPersonButton = require('../../../src/components/NewPersonButton');
+const PersonsCollection = require('../../../src/core/collections/persons');
+
+describe("components/NewPersonsButton", ()=> {
+
+    it("should not add person to collection", () => {
+
+        let persons = new PersonsCollection([
+            {name: 'Naomi'}
+        ]);
+
+        let btn = ReactTestUtils.renderIntoDocument(
+            <NewPersonButton />
+        );
+
+        let btnDOM = ReactTestUtils.findRenderedDOMComponentWithTag(btn, 'button');
+
+        ReactTestUtils.Simulate.click(btnDOM, {});
+
+        expect(persons.length).to.eql(1);
+
+    });
+
+    it("should add persons to collection", () => {
+
+        let testPersons = new PersonsCollection([
+            {name: 'Naomi'}
+        ]);
+
+        let btn = ReactTestUtils.renderIntoDocument(
+            <NewPersonButton collection={testPersons}/>
+        );
+
+        let btnDOM = ReactTestUtils.findRenderedDOMComponentWithTag(btn, 'button');
+
+        ReactTestUtils.Simulate.click(btnDOM, {});
+
+        expect(testPersons.length).to.eql(2);
+
+    });
+
+});
