28281a990025bfea3f6f685f7e2a0d7969136907
wailorman <wailorman@gmail.com>
2016-04-10 19:12:36 +0300

    chore(test fixtures): Move up

---
+++ public/test/fixtures/fake-state.js
@@ -0,0 +1,7 @@
+export const fakeState = {
+    products: {
+        1: {id:1, name: 'Water', price: '40'},
+        2: {id:2, name: 'Milk', price: '60'}
+    }
+};
+export default fakeState;
--- public/test/unit/fixtures/fake-state.js
+++
@@ -1,7 +0,0 @@
-export const fakeState = {
-    products: {
-        1: {name: 'Water', price: '40'},
-        2: {name: 'Milk', price: '60'}
-    }
-};
-export default fakeState;
