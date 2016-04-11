e8617dc82be7cc28e050237b940a4deb6b716e04
wailorman <wailorman@gmail.com>
2016-04-11 13:39:00 +0300

    fix(tests): window doesn't exist in cli mode

--- test/requirements/mocha.js
+++ test/requirements/mocha.js
@@ -1,3 +1,10 @@
 global.sinon = require('sinon');
 global.expect = require('chai').expect;
+
+try {
+    global.window = window ? window : {};
+} catch (e) {
+    global.window = {};
+}
+
 require('./universal');
