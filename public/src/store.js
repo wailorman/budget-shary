9dcbf62e0ab43973ebeccfe5cc233f5228eb9984
wailorman <wailorman@gmail.com>
2016-04-18 19:54:10 +0300

    fix(redux dev tools): Move window polyfill to store.js

--- public/src/store.js
+++ public/src/store.js
@@ -1,6 +1,12 @@
 import { createStore } from 'redux';
 import { reducers, defaultState } from './reducer'
 
+try {
+    global.window = window ? window : {};
+} catch (e) {
+    global.window = {};
+}
+
 export const store = createStore(
     reducers,
     defaultState,
--- test/requirements/mocha.js
+++ test/requirements/mocha.js
@@ -1,10 +1,4 @@
 global.sinon = require('sinon');
 global.expect = require('chai').expect;
 
-try {
-    global.window = window ? window : {};
-} catch (e) {
-    global.window = {};
-}
-
 require('./universal');
