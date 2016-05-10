dbe16e5921614d894d1631cc78607fd70e8c60b4
wailorman <wailorman@gmail.com>
2016-05-10 14:42:43 +0300

    fix(store): Initial persons IDs conflicts

--- public/src/reducer.js
+++ public/src/reducer.js
@@ -10,31 +10,31 @@ import { proceedInterchange } from './core/utils'
 export const defaultState = {
 
     persons: [
-        {id: '1', name: 'Jack', share: '30'},
-        {id: '2', name: 'Alice', share: '60'},
-        {id: '3', name: 'Mike', share: '10'}
+        {id: '_1', name: 'Jack', share: '30'},
+        {id: '_2', name: 'Alice', share: '60'},
+        {id: '_3', name: 'Mike', share: '10'}
     ],
     products: [
-        {id: '1', name: '',     price: '45',    ownerId: '1'},
-        {id: '2', name: '',     price: '234',   ownerId: '1'},
-        {id: '3', name: '',     price: '12',    ownerId: '1'},
-        {id: '4', name: '',     price: '89',    ownerId: '1'},
-        {id: '5', name: '',     price: '65',    ownerId: '1'},
-        {id: '6', name: '',     price: '234',   ownerId: '1'},
+        {id: '1', name: '',     price: '45',    ownerId: '_1'},
+        {id: '2', name: '',     price: '234',   ownerId: '_1'},
+        {id: '3', name: '',     price: '12',    ownerId: '_1'},
+        {id: '4', name: '',     price: '89',    ownerId: '_1'},
+        {id: '5', name: '',     price: '65',    ownerId: '_1'},
+        {id: '6', name: '',     price: '234',   ownerId: '_1'},
 
-        {id: '7', name: '',     price:  '345',  ownerId: '2'},
-        {id: '8', name: '',     price:  '234',  ownerId: '2'},
-        {id: '9', name: '',     price:  '890',  ownerId: '2'},
-        {id: '10', name: '',    price: '1234',  ownerId: '2'},
-        {id: '11', name: '',    price: '671',   ownerId: '2'},
-        {id: '12', name: '',    price: '55',    ownerId: '2'},
-        {id: '13', name: '',    price: '176',   ownerId: '2'},
-        {id: '14', name: '',    price: '1876',  ownerId: '2'},
+        {id: '7', name: '',     price:  '345',  ownerId: '_2'},
+        {id: '8', name: '',     price:  '234',  ownerId: '_2'},
+        {id: '9', name: '',     price:  '890',  ownerId: '_2'},
+        {id: '10', name: '',    price: '1234',  ownerId: '_2'},
+        {id: '11', name: '',    price: '671',   ownerId: '_2'},
+        {id: '12', name: '',    price: '55',    ownerId: '_2'},
+        {id: '13', name: '',    price: '176',   ownerId: '_2'},
+        {id: '14', name: '',    price: '1876',  ownerId: '_2'},
 
-        {id: '15', name: '',    price: '504',   ownerId: '3'},
-        {id: '16', name: '',    price: '646',   ownerId: '3'},
-        {id: '17', name: '',    price: '756',   ownerId: '3'},
-        {id: '18', name: '',    price: '50',    ownerId: '3'}
+        {id: '15', name: '',    price: '504',   ownerId: '_3'},
+        {id: '16', name: '',    price: '646',   ownerId: '_3'},
+        {id: '17', name: '',    price: '756',   ownerId: '_3'},
+        {id: '18', name: '',    price: '50',    ownerId: '_3'}
     ],
     transactions: []
 
