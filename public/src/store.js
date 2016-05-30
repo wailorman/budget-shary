004f823d320a83197c048a2d6f0cb3f49a771545
wailorman <wailorman@gmail.com>
2016-05-30 20:07:22 +0300

    feat(core/store): Redux DevTools for isomorphic apps

--- public/src/store.js
+++ public/src/store.js
@@ -2,18 +2,13 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { reducers, defaultState } from './reducer'
 
-try {
-    global.window = window ? window : {};
-} catch (e) {
-    global.window = {};
-}
 
 export const store = createStore(
     reducers,
     defaultState,
     compose(
         applyMiddleware(thunk)
-        // , window && window.devToolsExtension ? window.devToolsExtension() : function () {}
+        , typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
     )
 );
 
