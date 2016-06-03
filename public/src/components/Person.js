f58b5bf423a0a79f674214f6d673fc95b3830f75
wailorman <wailorman@gmail.com>
2016-06-03 19:09:27 +0300

    feat(<Person />): Rewrite to functional Redux container component

--- public/src/components/Person.js
+++ public/src/components/Person.js
@@ -1,67 +1,83 @@
 import ValidationErrorsList from './ValidationErrorsList'
+import {changePerson, removePerson} from '../actions'
+import {connect} from 'react-redux'
 
 import '../styles/Person.css'
 
-const Person = React.createClass({
-    propTypes: {
-        name: React.PropTypes.string,
-        share: React.PropTypes.string,
-        changePerson: React.PropTypes.func,
-        removePerson: React.PropTypes.func,
-        validationErrors: React.PropTypes.object
+export const Person = (props)=> (
+
+    <div className="Person">
+
+        <ValidationErrorsList errors={props.validationErrors} />
+
+        <input
+            className="Person__name-input"
+            type="text"
+            ref="name"
+            placeholder="Name"
+            value={props.name}
+            onChange={props.onChange}
+        />
+
+        <input
+            className="Person__share-input"
+            size="3"
+            type="text"
+            ref="share"
+            placeholder="Share"
+            value={props.share}
+            onChange={props.onChange}
+        />%
+
+        &nbsp;
+        <button className="Person__remove-button" onClick={props.onRemove}>
+            x
+        </button>
+    </div>
+    
+);
+
+Person.propTypes = {
+    id: React.PropTypes.oneOfType(React.PropTypes.string, React.PropTypes.number).isRequired,
+    name: React.PropTypes.string,
+    share: React.PropTypes.string,
+    onChange: React.PropTypes.func.isRequired,
+    onRemove: React.PropTypes.func.isRequired,
+    validationErrors: React.PropTypes.object
+};
+
+export default connect({
+    mapStateToProps: (state, ownProps) => {
+        return {
+            name: state.persons[ownProps.id].name,
+            share: state.persons[ownProps.id].share
+        }
     },
+    mapDispatchToProps: (dispatch, ownProps) => {
+        return {
+            onChange: (event) => {
 
-    onChange(event){
+                const initiatorClassName = event.target.className;
 
-        const targetClassName = event.target.className;
+                const name = initiatorClassName == 'Person__name-input' ?
+                    event.target.value : ownProps.name;
 
-        const name = targetClassName == 'Person__name-input' ? event.target.value : this.props.name;
-        const share = targetClassName == 'Person__share-input' ? event.target.value : this.props.share;
+                const share = initiatorClassName == 'Person__share-input' ?
+                    event.target.value : ownProps.share;
 
-        // Person__name-input
-        // Person__share-input
+                dispatch(
+                    changePerson(ownProps.id, {name, share})
+                );
 
-        this.props.changePerson({name, share});
+            },
+            onRemove: ()=> {
 
-    },
+                dispatch(
+                    removePerson(ownProps.id)
+                );
 
-    onRemove(){
-        this.props.removePerson();
-    },
-
-    render: function () {
-
-        return (
-            <div className="Person">
-                
-                <ValidationErrorsList errors={this.props.validationErrors} />
-
-                <input
-                    className="Person__name-input"
-                    type="text"
-                    ref="name"
-                    placeholder="Name"
-                    value={this.props.name}
-                    onChange={this.onChange}
-                />
-
-                <input
-                    className="Person__share-input"
-                    size="3"
-                    type="text"
-                    ref="share"
-                    placeholder="Share"
-                    value={this.props.share}
-                    onChange={this.onChange}
-                />%
-
-                &nbsp;
-                <button className="Person__remove-button" onClick={this.onRemove}>
-                    x
-                </button>
-            </div>
-        );
+            }
+        }
     }
-});
 
-export default Person;
+})(Person);
