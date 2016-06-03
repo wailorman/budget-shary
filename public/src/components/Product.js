4d853915c3c8a35c5ea8498198cc8d0ee3bcf357
wailorman <wailorman@gmail.com>
2016-06-03 09:31:46 +0300

    feat(<Product/>): Rewrite in Redux containers way

--- public/src/components/Product.js
+++ public/src/components/Product.js
@@ -1,62 +1,77 @@
 import ValidationErrorsList from './ValidationErrorsList'
+import {connect} from 'react-redux'
+import {changeProduct, removeProduct} from '../actions'
 
-const Product = React.createClass({
-    propTypes: {
-        name: React.PropTypes.string,
-        price: React.PropTypes.string,
-        changeProduct: React.PropTypes.func,
-        removeProduct: React.PropTypes.func,
-        validationErrors: React.PropTypes.object
+export const Product = (props)=> (
+    <div className="Product">
+
+        <ValidationErrorsList errors={props.validationErrors}/>
+
+        <input
+            className="Product__name-input"
+            size="17"
+            type="text"
+            ref="name"
+            value={props.name}
+            onChange={props.onChange}
+        />
+
+        <input
+            className="Product__price-input"
+            size="5"
+            type="text"
+            ref="price"
+            value={props.price}
+            onChange={props.onChange}
+        />
+
+        <button className="Product__remove-button" onClick={props.onRemove}>
+            x
+        </button>
+    </div>
+);
+
+Product.propTypes = {
+    id: React.PropTypes.oneOfType(React.PropTypes.string, React.PropTypes.number).isRequired,
+    name: React.PropTypes.string,
+    price: React.PropTypes.string,
+    onChange: React.PropTypes.func.isRequired,
+    onRemove: React.PropTypes.func.isRequired,
+    validationErrors: React.PropTypes.object
+};
+
+export const ProductContainer = connect({
+    mapStateToProps: (state, ownProps) => {
+        return {
+            name: state.products[ownProps.id].name,
+            price: state.products[ownProps.id].price
+        }
     },
+    mapDispatchToProps: (dispatch, ownProps) => {
+        return {
+            onChange: (event) => {
 
-    onChange(event){
+                const initiatorClassName = event.target.className;
 
-        const targetClassName = event.target.className;
+                const name = initiatorClassName == 'Product__name-input' ?
+                    event.target.value : ownProps.name;
 
-        const name = targetClassName == 'Product__name-input' ? event.target.value : this.props.name;
-        const price = targetClassName == 'Product__price-input' ? event.target.value : this.props.price;
+                const price = initiatorClassName == 'Product__price-input' ?
+                    event.target.value : ownProps.price;
 
-        // Product__name-input
-        // Product__price-input
+                dispatch(
+                    changeProduct(ownProps.id, {name, price})
+                );
 
-        this.props.changeProduct({name, price});
+            },
+            onRemove: ()=> {
 
-    },
+                dispatch(
+                    removeProduct(ownProps.id)
+                );
 
-    onRemoveClick(){
-        this.props.removeProduct();
-    },
-
-    render: function () {
-        return (
-            <div className="Product">
-
-                <ValidationErrorsList errors={this.props.validationErrors} />
-
-                <input
-                    className="Product__name-input"
-                    size="17"
-                    type="text"
-                    ref="name"
-                    value={this.props.name}
-                    onChange={this.onChange}
-                />
-
-                <input
-                    className="Product__price-input"
-                    size="5"
-                    type="text"
-                    ref="price"
-                    value={this.props.price}
-                    onChange={this.onChange}
-                />
-
-                <button className="Product__remove-button" onClick={this.onRemoveClick}>
-                    x
-                </button>
-            </div>
-        );
+            }
+        }
     }
-});
 
-export default Product;
+})(Product);
