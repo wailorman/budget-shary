83d17fc941d5c0731a46f290af40b884542668de
wailorman <wailorman@gmail.com>
2016-06-04 15:20:38 +0300

    feat(store): Store factory

--- public/src/store.js
+++ public/src/store.js
@@ -1,25 +1,27 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
-import { reducers, defaultState } from './reducer'
+import {reducers, defaultState} from './reducer'
 import {stateSyncMiddleware} from './core/state-sync-middleware'
 
+export const generateStore = (args = {}) => {
 
-export const store = createStore(
-    reducers,
-    defaultState,
-    compose(
-        applyMiddleware(
-            thunk,
-            stateSyncMiddleware(reducers)
+    _.defaults(args, {reducer: reducers, initialState: defaultState});
+
+    return createStore(
+        args.reducer,
+        args.initialState,
+        compose(
+            applyMiddleware(
+                thunk,
+                stateSyncMiddleware(reducers)
+            )
+            , typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
         )
-        , typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
-    )
-);
-
-export const generateStore = (initialState) => {
-
-    return createStore(reducers, initialState, applyMiddleware(thunk));
+    );
 
 };
+
+
+export const store = generateStore();
 
 export default store;
