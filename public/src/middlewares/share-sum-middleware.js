5e501fc6c064b27e15439cc8e96cf4a6f2f28d71
wailorman <wailorman@gmail.com>
2016-09-03 15:12:22 +0300

    feat(middlewares): Add share sum middleware
    This middleware will attach new sum of all persons shares to CHANGE_PERSON action like that:
    ```
    {
      type: CHANGE_PERSON,
      ...
      meta: {
        newShareSum: '100'
      }
    }
    ```

---
+++ public/src/middlewares/share-sum-middleware.js
@@ -0,0 +1,19 @@
+import {sumAllShares} from '../core/interchange-utils'
+import {CHANGE_PERSON} from '../actions'
+
+export const shareSumMiddleware = (reducer) => (store) => (next) => (action) => {
+
+    if (action.type != CHANGE_PERSON) return next(action);
+    
+
+    const nextState = reducer(store.getState(), action);
+
+    const newShareSum = sumAllShares(nextState.persons).toString();
+
+    let newAction = _.cloneDeep(action);
+
+    _.set(newAction, 'meta.newShareSum', newShareSum);
+
+    return next(newAction);
+    
+};
---
+++ public/test/unit/middlewares/share-sum-middleware.unit.js
@@ -0,0 +1,85 @@
+import {shareSumMiddleware} from '../../../src/middlewares/share-sum-middleware'
+import {reducer} from '../../../src/reducer'
+import {CHANGE_PERSON, CHANGE_PRODUCT} from '../../../src/actions'
+import {normalizedFakeState} from '../../fixtures/fake-state'
+
+describe("UNIT / Middlewares / share sum", ()=> {
+
+    const fakeStore = (initialState) => {
+
+        return {
+            getState: ()=> {
+                return initialState;
+            }
+        };
+
+    };
+
+    const store = fakeStore(normalizedFakeState);
+    
+    const callMiddleware = (action)=> {
+
+        const next = (action)=> {
+            return action;
+        };
+
+        return shareSumMiddleware(reducer)(store)(next)(action);
+
+    };
+
+    it(`should attach new shares sum into action`, () => {
+
+        const action = {
+            type: CHANGE_PERSON,
+            id: '1',
+            values: {
+                name: 'Mike',
+                share: '50'
+            }
+        };
+
+        const expected = {
+            type: CHANGE_PERSON,
+            id: '1',
+            values: {
+                name: 'Mike',
+                share: '50'
+            },
+            meta: {
+                newShareSum: '110'
+            }
+        };
+
+        const actual = callMiddleware(action);
+
+        expect(actual).to.eql(expected);
+
+    });
+    
+    it(`should not react on other actions`, () => {
+
+        const action = {
+            type: CHANGE_PRODUCT,
+            id: '1',
+            values: {
+                name: 'Milk',
+                price: '50'
+            }
+        };
+
+        const expected = {
+            type: CHANGE_PRODUCT,
+            id: '1',
+            values: {
+                name: 'Milk',
+                price: '50'
+            }
+        };
+
+        const actual = callMiddleware(action);
+
+        expect(actual).to.eql(expected);
+
+    });
+
+});
