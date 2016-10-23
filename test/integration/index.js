4a474f1aa46ee0e95eeaad8d5c7242f093862b03
wailorman <wailorman@gmail.com>
2016-10-23 10:50:23 +0300

    chore(test index files): Optimize scope of searching test files

--- test/integration/index.js
+++ test/integration/index.js
@@ -1,3 +1,3 @@
 function requireAll(r) { r.keys().forEach(r); }
-requireAll(require.context('../../src', true, /(test|unit|spec|int)\.js$/i));
-requireAll(require.context('../', true, /(test|unit|spec|int)\.js$/i));
+requireAll(require.context('../../src', true, /(int)\.js$/i));
+requireAll(require.context('./', true, /(int)\.js$/i));
--- test/unit/index.js
+++ test/unit/index.js
@@ -1,3 +1,3 @@
 function requireAll(r) { r.keys().forEach(r); }
 requireAll(require.context('../../src', true, /(unit)\.js$/i));
-requireAll(require.context('../', true, /(unit)\.js$/i));
+requireAll(require.context('./', true, /(unit)\.js$/i));
