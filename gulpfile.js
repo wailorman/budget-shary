eb4a3cd6f89a11aa4b9c46bf47cacda711fb2d4b
wailorman <wailorman@gmail.com>
2016-10-23 11:39:46 +0300

    chore(travis deploy): Deploying a zip archive, not js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,15 @@
 var gulp = require('gulp'),
-    plumber = require('gulp-plumber'),
-    less = require('gulp-less'),
-    concatCss = require('gulp-concat-css'),
-    source = require('vinyl-source-stream'),
-    copy = require('gulp-copy'),
-    watch = require('gulp-watch'),
-    clean = require('gulp-rimraf'),
+    zip = require('gulp-zip'),
+    packageInfo = require('./package.json');
 
-    browserify = require('browserify'),
-    babelify = require('babelify'),
-    watchify = require('watchify'),
+const zipName = `budget-shary-${packageInfo.version}.zip`;
 
-    // live reload
-    livereload = require('gulp-livereload'),
+gulp.task('zip', function () {
 
-// webpack
-    gutil = require('gulp-util'),
-    webpack = require('webpack'),
-    browserSync = require('browser-sync').create();
-
-var webpackConfig = require('./webpack.config');
-var NODE_ENV = 'production';
-
-/////////////    webpack    ///////////////
-
-gulp.task('set-dev-mode', function () {
-    NODE_ENV = 'development';
-});
-
-gulp.task("webpack", ['copy-index-html'], function (callback) {
-
-    if (NODE_ENV == 'development')
-        webpackConfig.watch = true;
-
-    // run webpack
-    webpack(webpackConfig, function (err, stats) {
-        if (err) throw new gutil.PluginError("webpack", err);
-        gutil.log("[webpack]", stats.toString({
-            // output options
-        }));
-        //callback();
-    });
-});
-
-gulp.task('copy-index-html', function () {
-
-    return gulp.src('./src/index.html')
-        .pipe(copy('./dist/', {prefix: 10}));
+    return gulp.src('dist/**/*')
+        .pipe(zip(zipName))
+        .pipe(gulp.dest('dist/'));
 
 });
-
-gulp.task('browser-sync', ['copy-index-html'], function () {
-    browserSync.init({
-        server: {
-            baseDir: "./dist/"
-        }
-    });
-
-    watch('./dist/**/*', function () {
-        browserSync.reload();
-    });
-    gulp.watch('./src/index.html', ['copy-index-html']);
-});
-
-gulp.task('serve', ['set-dev-mode', 'webpack', 'browser-sync']);
-
-/////////////    /webpack    ///////////////
 
 gulp.task('default', ['webpack']);
