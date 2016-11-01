3858ea5df94f69a71efc52c59b2db1c8986311c3
wailorman <wailorman@gmail.com>
2016-11-01 22:09:14 +0300

    feat: Split Budget component & Budget container

---
+++ src/components/Budget.js
@@ -0,0 +1,98 @@
+import {bindActionCreators} from 'redux';
+
+import Product from '../components/Product';
+import Person from '../components/Person';
+import ValidationErrorsList from '../components/ValidationErrorsList';
+import TransactionsList from '../components/TransactionsList';
+import BudgetName from '../components/BudgetName';
+import ParticipatingRow from '../components/ParticipatingRow';
+import NewProductButton from '../components/NewProductButton';
+
+import {getProductsByPersonId} from '../core/components-utils';
+
+import * as actionCreators from '../actions';
+
+
+export function Budget ({state, dispatch}) {
+
+    const actions = bindActionCreators(actionCreators, dispatch);
+
+    return (
+        <div>
+
+            <BudgetName name={state.budget.name}
+                        onChange={actions.changeBudgetProps.bind(null)} />
+
+            {_.map(state.persons, (person)=> {
+
+
+                const ownProducts = getProductsByPersonId(person.id, state.products);
+                const ownProductsIds = _.map(ownProducts, 'id');
+
+
+                const ownErrors = _.get(state, `errors.persons[${person.id}]`, {});
+                const ownProductsErrors = _.chain(state.errors.products).pick(ownProductsIds).value();
+
+
+                return (
+
+                    <Person
+                        key={person.id}
+                        name={person.name}
+                        share={person.share}
+                        validationErrors={ownErrors}
+
+                        onChange={actions.changePerson.bind(null, person.id)}
+                        onRemove={actions.removePerson.bind(null, person.id)}
+                    >
+
+                        {_.map(ownProductsIds, (productId)=>(
+
+                            <Product
+                                key={productId}
+                                name={state.products[productId].name}
+                                price={state.products[productId].price}
+                                validationErrors={ownProductsErrors[productId]}
+
+                                onChange={actions.changeProduct.bind(null, productId)}
+                                onRemove={actions.removeProduct.bind(null, productId)}
+                            >
+
+                                <ParticipatingRow
+                                    productParticipatingElem={state.productParticipating ? state.productParticipating[productId] || {} : {}}
+                                    onClick={actions.toggleParticipation.bind(null, productId)}
+                                    persons={state.persons}
+                                />
+
+                            </Product>
+
+                        ))}
+
+                        <NewProductButton onClick={actions.newProduct.bind(null, person.id)} />
+
+                    </Person>
+
+                );
+
+            })}
+
+            <button onClick={actions.newPerson}>{"New person"}</button>
+
+            <ValidationErrorsList errors={state.errors.common}/>
+
+            <br /><br />
+
+            <button onClick={actions.realizeInterchange}>Calculate</button>
+
+            <TransactionsList transactions={state.transactions}/>
+
+        </div>
+    );
+}
+
+Budget.propTypes = {
+    state: React.PropTypes.object.isRequired,
+    dispatch: React.PropTypes.func.isRequired
+};
+
+export default Budget;
--- src/containers/BudgetContainer.js
+++ src/containers/BudgetContainer.js
@@ -1,121 +1,28 @@
-import React from 'react';
-
 import {STUB_BUDGET_ID} from '../state-stub';
 
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
 
-import Product from '../components/Product';
-import Person from '../components/Person';
-import ValidationErrorsList from '../components/ValidationErrorsList';
-import TransactionsList from '../components/TransactionsList';
-import BudgetName from '../components/BudgetName';
-import ParticipatingRow from '../components/ParticipatingRow';
-import NewProductButton from '../components/NewProductButton';
+import Budget from '../components/Budget';
 
-import {getProductsByPersonId} from '../core/components-utils';
-
-import * as actionCreators from '../actions';
+import {fetchBudget} from '../actions';
 
 
-export function BudgetComponent ({state, dispatch}) {
-
-    const actions = bindActionCreators(actionCreators, dispatch);
-
-    return (
-        <div>
-
-            <BudgetName name={state.budget.name}
-                        onChange={actions.changeBudgetProps.bind(null)} />
-
-            {_.map(state.persons, (person)=> {
-
-
-                const ownProducts = getProductsByPersonId(person.id, state.products);
-                const ownProductsIds = _.map(ownProducts, 'id');
-
-
-                const ownErrors = _.get(state, `errors.persons[${person.id}]`, {});
-                const ownProductsErrors = _.chain(state.errors.products).pick(ownProductsIds).value();
-
-
-                return (
-
-                    <Person
-                        key={person.id}
-                        name={person.name}
-                        share={person.share}
-                        validationErrors={ownErrors}
-
-                        onChange={actions.changePerson.bind(null, person.id)}
-                        onRemove={actions.removePerson.bind(null, person.id)}
-                    >
-
-                        {_.map(ownProductsIds, (productId)=>(
-
-                            <Product
-                                key={productId}
-                                name={state.products[productId].name}
-                                price={state.products[productId].price}
-                                validationErrors={ownProductsErrors[productId]}
-
-                                onChange={actions.changeProduct.bind(null, productId)}
-                                onRemove={actions.removeProduct.bind(null, productId)}
-                            >
-                                
-                                <ParticipatingRow
-                                    productParticipatingElem={state.productParticipating ? state.productParticipating[productId] || {} : {}}
-                                    onClick={actions.toggleParticipation.bind(null, productId)}
-                                    persons={state.persons}
-                                />
-                                
-                            </Product>
-
-                        ))}
-
-                        <NewProductButton onClick={actions.newProduct.bind(null, person.id)} />
-
-                    </Person>
-
-                );
-
-            })}
-
-            <button onClick={actions.newPerson}>{"New person"}</button>
-
-            <ValidationErrorsList errors={state.errors.common}/>
-
-            <br /><br />
-
-            <button onClick={actions.realizeInterchange}>Calculate</button>
-
-            <TransactionsList transactions={state.transactions}/>
-
-        </div>
-    );
-}
-
-BudgetComponent.propTypes = {
-    state: React.PropTypes.object.isRequired,
-    dispatch: React.PropTypes.func.isRequired
-};
-
-class Budget extends React.Component {
+class BudgetContainer extends React.Component {
 
     componentDidMount(){
 
         const budgetIdToFetch = this.props.params.id || STUB_BUDGET_ID;
 
-        this.props.dispatch(actionCreators.fetchBudget(budgetIdToFetch));
+        this.props.dispatch(fetchBudget(budgetIdToFetch));
     }
 
     render() {
-        return (<BudgetComponent {...this.props} />);
+        return (<Budget {...this.props} />);
     }
 
 }
 
-Budget.propTypes = {
+BudgetContainer.propTypes = {
     dispatch: React.PropTypes.func.isRequired,
     params: React.PropTypes.object
 };
@@ -127,4 +34,4 @@ export default connect(
     (dispatch)=> {
         return {dispatch};
     }
-)(Budget);
+)(BudgetContainer);
