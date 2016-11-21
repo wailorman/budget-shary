24bf6a26b547fdcd746e896283924cb853f3a0b1
wailorman <wailorman@gmail.com>
2016-11-21 23:01:16 +0300

    feat(<Product/>): Remove onBlur changes

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,118 +6,83 @@ import FontIcon from 'material-ui/FontIcon';
 
 import "../styles/Product.css";
 
-class Product extends React.Component {
+export const Product = (props)=> {
 
-    constructor(props){
-        super(props);
+    const onChange = (propName)=>(event) => {
 
-        this.state = {
-            name: '',
-            price: ''
-        };
-    }
+        const newValue = event.target.value;
 
-    render() {
-
-        const props = this.props;
-
-        const onChange = (propName)=>(event) => {
-
-            const newValue = event.target.value;
-
-            const isChangedName = propName == 'name';
-            const isChangedPrice = propName == 'price';
+        const isChangedName = propName == 'name';
+        const isChangedPrice = propName == 'price';
 
 
-            const name = isChangedName ? newValue : props.name;
-            const price = isChangedPrice ? newValue : props.price;
+        const name = isChangedName ? newValue : props.name;
+        const price = isChangedPrice ? newValue : props.price;
 
-            this.setState({name, price});
+        props.onChange({name, price});
 
-        };
+    };
 
-        const onBlur = (propName)=> (event)=> {
+    const onRemove = ()=> {
+        props.onRemove();
+    };
 
-            const newValue = event.target.value;
+    return (
+        <div className="Product">
 
-            const isChangedName = propName == 'name';
-            const isChangedPrice = propName == 'price';
+            <div className="Product__inputs-and-participating">
 
+                <div className="Product__participating">
 
-            const name = isChangedName ? newValue : props.name;
-            const price = isChangedPrice ? newValue : props.price;
-
-            props.onChange({name, price});
-
-        };
-
-        const onRemove = ()=> {
-            props.onRemove();
-        };
-
-        const name = this.state.name || props.name;
-        const price = this.state.price || props.price;
-
-        return (
-            <div className="Product">
-
-                <div className="Product__inputs-and-participating">
-
-                    <div className="Product__participating">
-
-                        {props.children}
-
-                    </div>
-
-                    <div className="Product__inputs">
-
-                        <TextField
-                            style={{
-                            fontSize: '0.95rem',
-                            width: null
-                        }}
-                            className="Product__name-input"
-                            hintText="Name"
-                            value={name}
-                            onChange={onChange('name')}
-                            onBlur={onBlur('name')}
-                            errorText={_.get(props, 'validationErrors.name', []).join(', ')}
-                        />
-
-                        <TextField
-                            style={{
-                            fontSize: '0.95rem',
-                            width: null
-                        }}
-                            className="Product__price-input"
-                            hintText="Price"
-                            value={price}
-                            onChange={onChange('price')}
-                            onBlur={onBlur('price')}
-                            errorText={_.get(props, 'validationErrors.price', []).join(', ')}
-                        />
-
-                    </div>
+                    {props.children}
 
                 </div>
 
-                <FlatButton
+                <div className="Product__inputs">
 
+                    <TextField
+                        style={{
+                            fontSize: '0.95rem',
+                            width: null
+                        }}
+                        className="Product__name-input"
+                        hintText="Name"
+                        value={props.name}
+                        onChange={onChange('name')}
+                        errorText={_.get(props, 'validationErrors.name', []).join(', ')}
+                    />
 
-                    className="Product__remove-button"
-                    onClick={onRemove}
-                    icon={<FontIcon className="material-icons">clear</FontIcon>}
+                    <TextField
+                        style={{
+                            fontSize: '0.95rem',
+                            width: null
+                        }}
+                        className="Product__price-input"
+                        hintText="Price"
+                        value={props.price}
+                        onChange={onChange('price')}
+                        errorText={_.get(props, 'validationErrors.price', []).join(', ')}
+                    />
 
-                    style={{
-                        minWidth: 40
-                    }}
-                />
+                </div>
 
             </div>
-        );
 
-    }
-}
+            <FlatButton
+
+
+                className="Product__remove-button"
+                onClick={onRemove}
+                icon={<FontIcon className="material-icons">clear</FontIcon>}
+
+                style={{
+                    minWidth: 40
+                }}
+            />
+
+        </div>
+    );
+};
 
 Product.propTypes = {
     name: React.PropTypes.string,
