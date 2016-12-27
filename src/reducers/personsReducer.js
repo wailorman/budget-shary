7651e3a9f0c3f9f4210b881ab51ce19c82096d58
wailorman <wailorman@yandex.ru>
2016-12-27 19:24:48 +0300

    refactor(reducers/persons): Remove lodash

--- src/reducers/personsReducer.js
+++ src/reducers/personsReducer.js
@@ -9,44 +9,65 @@ import {
 import {initialState} from './initial-state';
 
 export function personsReducer(state = initialState.persons, action = {}) {
-    let newState = _.cloneDeep(state);
 
     switch (action.type) {
 
         case FETCH_BUDGET:
         {
-            return _.get(action, 'result.persons', initialState.persons);
+            return {
+                ...((action.result || {}).persons || initialState.persons)
+            };
         }
 
         case REMOVE_PERSON:
         {
-            if (newState[action.id]) {
-                delete newState[action.id];
-                return newState;
-            }else{
-                return state;
-            }
+
+            if (!state[action.id]) return state;
+
+            return Object
+                .keys(state)
+                .map(personId => state[personId])
+                .reduce((result, currentPerson) => {
+
+                    if (currentPerson.id != action.id){
+                        result = {
+                            ...result,
+                            [currentPerson.id]: {
+                                ...currentPerson
+                            }
+                        };
+                    }
+
+                    return result;
+
+                }, {});
         }
 
         case NEW_PERSON:
         {
-            if ( !action.id || state[action.id] )
-                return state;
+            if ( !action.id || state[action.id] ) return state;
 
-            newState[action.id] = {id: action.id, name: '', share: ''};
-
-            return newState;
+            return {
+                ...state,
+                [action.id]: {
+                    id: action.id,
+                    name: '',
+                    share: ''
+                }
+            };
         }
 
         case CHANGE_PERSON:
         {
-            if (!newState[action.id]) return state;
+            if (!state[action.id]) return state;
 
-            const consideringPerson = newState[action.id];
-
-            newState[action.id] = _.assign(consideringPerson, action.values);
-
-            return newState;
+            return {
+                ...state,
+                [action.id]: {
+                    ...state[action.id],
+                    ...action.values
+                }
+            };
         }
 
         case TOGGLE_PARTICIPATION:
@@ -56,11 +77,19 @@ export function personsReducer(state = initialState.persons, action = {}) {
                 action.productId && action.personId
             ) {
 
-                _.forOwn(newState, (person, personId)=> {
-                    person.share = action.meta.newPersonShares[personId] || 0;
-                });
+                return Object
+                    .keys(state)
+                    .reduce((result, personId) => {
+                        const person = state[personId];
 
-                return newState;
+                        result[personId] = {
+                            ...person,
+                            share: action.meta.newPersonShares[personId] || 0
+                        };
+
+                        return result;
+
+                    }, {});
 
             }
 
