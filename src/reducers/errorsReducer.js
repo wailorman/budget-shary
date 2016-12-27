e770594d2eda67f3b0e1e19218fa1e1e5d2394e3
wailorman <wailorman@yandex.ru>
2016-12-27 20:27:55 +0300

    refactor(reducers/errorsReducer): Remove lodash

--- src/reducers/errorsReducer.js
+++ src/reducers/errorsReducer.js
@@ -5,12 +5,7 @@ export function errorsReducer(state = initialState.errors, action = {}) {
     if (action.meta && action.meta.errors) {
         return action.meta.errors;
     } else {
-
-        // clearing errors state
-        if (_.isEqual(state, initialState.errors))
-            return state;
-        else
-            return initialState.errors;
+        return initialState.errors;
     }
 
 }
