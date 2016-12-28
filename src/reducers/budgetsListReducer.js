7a16ce3bf9232b5e4e3547681464085cdde830da
wailorman <wailorman@yandex.ru>
2016-12-28 22:00:38 +0300

    refactor(reducers/budgetsListReducer): Implement Immutable.js

--- src/reducers/budgetsListReducer.js
+++ src/reducers/budgetsListReducer.js
@@ -5,25 +5,23 @@ import {
 
 import {initialState} from './initial-state';
 
+import * as Immutable from 'immutable';
+
 export function budgetsListReducer(state = initialState.budgetsList, action) {
     switch (action.type) {
         // todo: Cover with tests
         case FETCH_BUDGETS_LIST:
         {
-            return {
-                ...((action.result || {}) || initialState.budgetsList)
-            };
+            return Immutable.Map(action.result).toJS();
         }
         case DELETE_BUDGET:
         {
             if (action.success){
-                return Object
-                    .keys(state)
-                    .filter(budgetId => budgetId != action.id)
-                    .reduce((result, currentId) => ({
-                        ...result,
-                        [currentId]: state[currentId]
-                    }), {});
+
+                return Immutable.Map(state)
+                    .filterNot((budget, id) => id == action.id)
+                    .toJS();
+
             } else {
                 return state;
             }
