0ce316ff3a56cc6b2b1ee2730f19f628d48ab9ae
wailorman <wailorman@yandex.ru>
2017-02-25 03:19:08 +0300

    refactor(Members): Change extension

---
+++ src/containers/Members.js
@@ -0,0 +1,50 @@
+import React, { PropTypes } from 'react';
+
+import { connect } from 'react-redux';
+import TextField from 'material-ui/TextField';
+
+import { budgetNameSelector } from '../selectors/budget';
+import { personsListSelector } from '../selectors/persons';
+import { fetchBudget, changeBudgetProps } from '../actions';
+import '../styles/Members.css';
+
+@connect((state, ownProps) => {
+    return {
+        budgetId: ownProps.params.id,
+        budgetName: budgetNameSelector(state),
+        personsList: personsListSelector(state)
+    };
+},
+(dispatch, ownProps) => {
+    return {
+        fetchBudget: () => dispatch(fetchBudget(ownProps.params.id)),
+        changeBudgetProps: (event) => dispatch(changeBudgetProps({name: event.target.value}))
+    };
+})
+export default class Members extends React.Component {
+    static propTypes = {
+        budgetId: PropTypes.string,
+        budgetName: PropTypes.string,
+        fetchBudget: PropTypes.func,
+        changeBudgetProps: PropTypes.func
+    }
+
+    componentDidMount() {
+        this.props.fetchBudget();
+    }
+
+    render() {
+        const props = this.props;
+        console.log('this.props.budgetName', this.props.budgetName);
+        return (
+            <div className="Members">
+                <p>Specify budget name</p>
+                <TextField
+                    hintText="Budget name"
+                    value={props.budgetName}
+                    onChange={props.changeBudgetProps}
+                    fullWidth={true}/>
+            </div>
+        );
+    }
+}
