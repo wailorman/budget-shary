9088c8d7f98d77f854e51d873a55a10844688ca9
wailorman <wailorman@yandex.ru>
2017-02-26 03:01:28 +0300

    fix(ParticipatingRow): Better data selection

--- src/components/ParticipatingRow.js
+++ src/components/ParticipatingRow.js
@@ -1,36 +1,35 @@
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import {pure} from 'recompose';
 
 import ParticipatingSwitcher from './ParticipatingSwitcher';
 
 import '../styles/ParticipatingRow.css';
 
-import * as definedPropTypes from '../reducers/prop-types';
 import {toggleParticipation} from '../actions';
+
+import {productParticipatingSelector} from '../selectors/participating';
 
 
 @connect(
     (state, {productId}) => {
         return {
-            productParticipatingElem: _.get(state, `productParticipating[${productId}]`, null),
-            persons: _.map(state.persons, ({id, name})=> ({id, name}) )
+            participating: productParticipatingSelector(productId)(state)
         };
     },
     (dispatch, {productId}) => {
         return {
             onClick: (personId) => () =>
-                bindActionCreators(toggleParticipation, dispatch)(productId, personId)
+                dispatch(toggleParticipation(productId, personId))
         };
     }
 )
-export class ParticipatingRow extends React.Component {
+@pure
+export default class ParticipatingRow extends React.Component {
 
-    shouldComponentUpdate(nextProps) {
-
-        const shouldUpdate =    !_.isEqual(this.props.persons, nextProps.persons) ||
-                                !_.isEqual(this.props.productParticipatingElem, nextProps.productParticipatingElem);
-
-        return shouldUpdate;
+    static propTypes = {
+        onClick: React.PropTypes.func.isRequired,
+        participating: React.PropTypes.array,
+        productId: React.PropTypes.string
     }
 
     render() {
@@ -40,17 +39,15 @@ export class ParticipatingRow extends React.Component {
         return (
             <div className="ParticipatingRow">
 
-                {_.map(props.persons, (person) => {
+                {this.props.participating.map(([personId, personName, state]) => {
 
-                    const switcherState = _.get(props, `productParticipatingElem[${person.id}]`, false);
-
-                    return person.name ?
+                    return personName ?
                         (
                             <ParticipatingSwitcher
-                                key={person.id}
-                                personName={person.name}
-                                state={switcherState}
-                                onClick={props.onClick(person.id)}
+                                key={personId}
+                                label={personName}
+                                enabled={state}
+                                onClick={props.onClick(personId)}
                             />
                         )
                         :
@@ -63,21 +60,4 @@ export class ParticipatingRow extends React.Component {
 
     }
 
-    static propTypes = {
-        onClick: React.PropTypes.func,
-        persons: React.PropTypes.arrayOf(
-            React.PropTypes.shape({
-                id: React.PropTypes.string,
-                name: React.PropTypes.string
-            })
-        ),
-
-
-        productParticipatingElem: definedPropTypes.productParticipatingElem,
-
-        productId: definedPropTypes.id
-    }
-
 }
-
-export default ParticipatingRow;
